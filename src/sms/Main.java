9b5a7eb51a29229e0c1bab0bc4a021c0d6fc87e7
BIPIN0852 <bipinaryal0852@gmail.com>
2025-01-22 13:33:29 +0545

    Date and time added on the main code

--- src/sms/Main.java
+++ src/sms/Main.java
@@ -1,10 +1,14 @@
 package sms;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Student Management System");
-        System.out.println("Student Management System of herald college");
+        System.out.println("Welcome to the Student Management System of Herald College!");
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("Current date and time: " + dtf.format(now));
     }
 }
-
- 
